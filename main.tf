f2d106716a7c208b074316673e77d1a31861e85f
Harisha G <hhgsharish@gmail.com>
2025-04-12 17:23:07 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  access_key = "AKIAVFIWISDTTM3RH7L6"
+  secret_key = "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"
+  region = var.aws_region
+}
+
+
+# Data source to fetch the latest Ubuntu 22.04 AMI
+data "aws_ami" "ubuntu_22_04" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+#Key for ec2
+resource "tls_private_key" "k8s_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "k8s_key_pair" {
+  key_name   = "k8s-key" # You can give it a better name
+  public_key = tls_private_key.k8s_ssh_key.public_key_openssh
+}
+
